30b827034a725759c0e92afa72623a1d39732dcd
Akijunior <suitsu19@gmail.com>
2016-12-23 22:32:11 -0200

    Create Cap03_Q01.java

---
+++ Cap03_Q01.java
@@ -0,0 +1,40 @@
+package Cap03;
+
+import javax.swing.JOptionPane;
+
+public class Cap03_Q01 {
+	
+	public static void main(String[] args) {
+		
+		double valorProduto, desconto=0;
+		String nomeProduto;
+		nomeProduto = JOptionPane.showInputDialog("Digite o nome do produto: ");
+		valorProduto = Long.parseLong(JOptionPane.showInputDialog("Digite o valor do produto: "));
+		if (valorProduto>= 50 && valorProduto < 200) {
+			desconto = 5;
+		}
+		
+		else if (valorProduto>= 200 && valorProduto < 500) {
+			desconto = 6;
+		}
+		
+		else if (valorProduto>= 500 && valorProduto < 1000) {
+			desconto = 7;
+		}
+		
+		else if (valorProduto>= 1000) {
+			desconto = 8;
+		}
+		
+		else if (valorProduto < 0) {
+			System.out.println("Valor inválido!");
+		}
+		double precoFinal = valorProduto - (valorProduto * desconto/100);
+		System.out.println("Nome do Produto: " + nomeProduto);
+		System.out.println("Preço Original: " + valorProduto);
+		System.out.println("Preço Final com desconto: " + precoFinal);
+
+}
+
+
+}
