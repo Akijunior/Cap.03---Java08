a790a1d87c10f90dac2162a8a00c1ca2e7adce56
Akijunior <suitsu19@gmail.com>
2016-12-23 23:18:11 -0200

    Create Cap03_Q04.java

---
+++ Cap03_Q04.java
@@ -0,0 +1,15 @@
+package Cap03;
+
+import javax.swing.JOptionPane;
+
+public class Cap03_Q04 {
+	
+	public static void main(String[] args) {
+		
+		int valor = Integer.valueOf(JOptionPane.showInputDialog("Forneça um número: "));
+		JOptionPane.showMessageDialog(null, valor + " * 1 = " + valor * 1 + ".\n" + valor + " * 2 = " + valor * 2 + ".\n" + valor + " * 3 = " + valor * 3 + ".\n" + valor + " * 4 = " + valor * 4 + ".\n" + valor + " * 5 = " + valor * 5 + ".\n" + valor + " * 6 = " + valor * 6 + ".\n" + valor + " * 7 = " + valor * 7 + ".\n" + valor + " * 8 = " + valor * 8 + ".\n" + valor + " * 9 = " + valor * 9 + ".\n" + valor + " * 10 = " + valor * 10 + ".\n");
+		
+	}
+	
+
+}
